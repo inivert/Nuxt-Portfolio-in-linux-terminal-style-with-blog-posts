3c154f3be3fc5dbdff5f292bbd82c1d3b18f7989
inivert <inivert@duck.com>
2025-02-15 00:02:23 -0500

    Upgrade Tailwind CSS and PostCSS dependencies
    - Update Tailwind CSS to version 4.0.6
    - Bump PostCSS to version 8.5.2
    - Upgrade Autoprefixer to version 10.4.20
    - Configure Tailwind content paths for Vue and Nuxt files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,14 @@ import type { Config } from 'tailwindcss'
 
 export default {
   darkMode: 'class',
-  content: [],
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue'
+  ],
   theme: {
     extend: {
       colors: {
